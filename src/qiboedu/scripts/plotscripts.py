ca8bfd94b9d24332837eab0b49cfceb4048b58a5
MatteoRobbiati <robbiati.matteo@gmail.com>
2024-04-04 12:09:14 +0200

    add plotting function used in vqe example

--- src/qiboedu/scripts/plotscripts.py
+++ src/qiboedu/scripts/plotscripts.py
@@ -162,3 +162,32 @@ def plot_density_matrix(state):
     plt.yticks(ticks=np.arange(0,2**nqubits,1), labels=bitstrings)
     plt.colorbar()
     plt.show()  
+
+
+def plot_vqe_states(state, state2=None):
+    """
+    Plot `state` and `state2` if provided. 
+    # TODO: merge this function into one of the others.
+    """
+    n = int(np.log2(len(state)))
+    bitstrings = []
+    for i in range(2**n):
+        bitstrings.append(format(i, f"0{n}b"))
+    for i, amp in enumerate(state):
+        if i == 0:
+            plt.bar(bitstrings[i], np.abs(amp)**2, color='#C194D8', alpha=0.7, edgecolor="black", label="Ground state")
+        else:
+            plt.bar(bitstrings[i], np.abs(amp)**2, color='#C194D8', alpha=0.7, edgecolor="black")
+    if state2 is not None:     
+        for i, amp in enumerate(state2):
+            if i == 0:
+                plt.bar(bitstrings[i], np.abs(amp)**2, color='black', alpha=1, edgecolor="black", hatch="\\\\", facecolor="none", label="VQE approximation")
+            else:
+                plt.bar(bitstrings[i], np.abs(amp)**2, color='black', alpha=1, edgecolor="black", hatch="\\\\", facecolor="none")
+                
+    plt.xticks(rotation=90)
+    plt.xlabel("Components")
+    plt.ylabel("Probabilities")
+    plt.title("State representation")
+    plt.legend()
+    plt.show()
